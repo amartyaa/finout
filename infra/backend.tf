22b557e12d110e40ec7ba448f17167c78f28e24c
Amartya Anshuman <amartyanshuman.thakur@gmail.com>
2026-03-11 01:47:09 +0530

    Update README with Azure account limit note (#1)
    * fix: update secret handling and improve readability in Terraform configurations
    
    * upgraded tf version

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-finops-tfstate"
-    storage_account_name = "finopstfstate"
+    storage_account_name = "finopstfstatef3af"
     container_name       = "tfstate"
     key                  = "finops.terraform.tfstate"
   }
